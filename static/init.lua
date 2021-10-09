22feb112d6632f026392ca1c7d7381e977724b06
Jack <git@5079.ml>
2021-10-09 15:39:23 -0400

    SO DANK

--- static/init.lua
+++ static/init.lua
@@ -104,12 +104,195 @@ gui.ResetOnSpawn = false
 gui.DisplayOrder = (2 ^ 31) - 1
 API.parent(gui)
 
+-- GUI
+function create(className, defaultParent)
+	return function(propertyList)
+		local object = Instance.new(className)
+		local parent = nil
+
+		for index, value in next, propertyList do
+			if type(index) == 'string' then
+				if index == 'Parent' then
+					parent = value
+				else
+					object[index] = value
+				end
+			else
+				local valueType = typeof(value)
+				if valueType == 'function' then
+					value(object)
+				elseif valueType == 'Instance' then
+					value.Parent = object
+				elseif valueType == 'EnumItem' then
+					object[tostring(value.EnumType)] = value
+				end
+			end
+		end
+
+		if parent then
+			object.Parent = parent
+		elseif defaultParent then
+			object.Parent = defaultParent
+		end
+
+		return object
+	end
+end
+local Frame = create'Frame'
+local taskbar = Frame{
+	Position = UDim2.fromOffset(0, loaded and 0 or 100),
+	Enum.AutomaticSize.X,
+	Size = UDim2.new(1, 0, 0, 200),
+	BackgroundColor3 = colors.bg,
+	create'UICorner'{ CornerRadius = UDim.new(0, 30) },
+	create'UIListLayout'{
+		Enum.FillDirection.Horizontal,
+		Enum.HorizontalAlignment.Center,
+		Enum.VerticalAlignment.Center,
+		Padding = UDim.new(0, 10)
+	},
+	create'UIPadding'{
+		PaddingBottom = UDim.new(0.5),
+		PaddingLeft = UDim.new(0, 32),
+		PaddingRight = UDim.new(0, 32)
+	}
+}
+
+Frame{
+	AnchorPoint = Vector2.new(0.5, 1),
+	Position = UDim2.fromScale(0.5, 1),
+	Size = UDim2.new(0.5, 0, 0, 100),
+	Parent = gui,
+	BackgroundTransparency = 1,
+	taskbar
+}
+
 function open()
 	API.play(assets.open)
+	wrapper.MouseEnter:Connect(function()
+		game:GetService('TweenService'):Create(
+			taskbar,
+			TweenInfo.new(0.3, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out),
+			{ Position = UDim2.fromOffset(0, 0) }
+		):Play()
+		local i = 0
+		for _, v in ipairs(taskbar:GetChildren()) do
+			if v:IsA('GuiObject') then
+				game:GetService('TweenService'):Create(
+					v.btn,
+					TweenInfo.new(
+						0.3,
+						Enum.EasingStyle.Cubic,
+						Enum.EasingDirection.Out,
+						0,
+						false,
+						0.3 + (i * 0.1)
+					),
+					{ Position = UDim2.fromOffset(0, 0) }
+				):Play()
+				i = i + 1
+			end
+		end
+	end)
 end
 function close()
 	API.play(assets.close)
+	local i = 0
+	for _, v in ipairs(taskbar:GetChildren()) do
+		if v:IsA('GuiObject') then
+			game:GetService('TweenService'):Create(
+				v.btn,
+				TweenInfo.new(
+					0.3,
+					Enum.EasingStyle.Cubic,
+					Enum.EasingDirection.In,
+					0,
+					false,
+					i * 0.1
+				),
+				{ Position = UDim2.fromOffset(0, 100) }
+			):Play()
+			i = i + 1
+		end
+	end
+
+	game:GetService('TweenService'):Create(
+		taskbar,
+		TweenInfo.new(
+			0.3,
+			Enum.EasingStyle.Cubic,
+			Enum.EasingDirection.In,
+			0,
+			false,
+			(i + 1) * 0.1
+		),
+		{ Position = UDim2.fromOffset(0, 100) }
+	):Play()
 end
+
+function btn(name, texture, clicked)
+	local wrapper = Instance.new('Frame', taskbar)
+	wrapper.Size = UDim2.fromOffset(0, 50)
+	wrapper.AutomaticSize = Enum.AutomaticSize.X
+	wrapper.BackgroundTransparency = 1
+	wrapper.Name = name
+	local button = Instance.new('TextButton', wrapper)
+	button.MouseButton1Click:Connect(clicked)
+	button.BackgroundColor3 = colors.bg
+	button.Name = 'btn'
+	button.AutomaticSize = Enum.AutomaticSize.X
+	button.Size = UDim2.fromOffset(50, 50)
+	button.Text = ''
+	Instance.new('UICorner', button).CornerRadius = UDim.new(0, 20)
+	local padding = Instance.new('UIPadding', button)
+	padding.PaddingLeft = UDim.new(0, 10)
+	padding.PaddingRight = UDim.new(0, 10)
+	local label = Instance.new('TextLabel', button)
+	label.AutomaticSize = Enum.AutomaticSize.X
+	label.Text = name
+	label.Font = Enum.Font.GothamBlack
+	label.TextXAlignment = Enum.TextXAlignment.Left
+	label.TextSize = 1
+	label.TextTransparency = 1
+	label.BackgroundTransparency = true
+	label.TextColor3 = Color3.new(1, 1, 1)
+	label.Position = UDim2.new(0, 0, 0.5, 0)
+	label.AnchorPoint = Vector2.new(0, 0.5)
+	local icon = Instance.new('ImageLabel', button)
+	icon.ScaleType = Enum.ScaleType.Fit
+	icon.Image = texture
+	icon.BackgroundTransparency = 1
+	icon.Size = UDim2.fromOffset(32, 32)
+	icon.Position = UDim2.fromScale(0, 0.5)
+	icon.AnchorPoint = Vector2.new(0, 0.5)
+	button.MouseEnter:Connect(function()
+		game:GetService('TweenService'):Create(
+			label,
+			TweenInfo.new(0.3, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out),
+			{
+				TextSize = 20,
+				TextTransparency = 0,
+				Position = UDim2.new(0, 40, 0.5, 0)
+			}
+		):Play()
+	end)
+
+	button.MouseLeave:Connect(function()
+		game:GetService('TweenService'):Create(
+			label,
+			TweenInfo.new(0.3, Enum.EasingStyle.Cubic, Enum.EasingDirection.In),
+			{
+				TextSize = 0,
+				TextTransparency = 1,
+				Position = UDim2.fromScale(0, 0.5)
+			}
+		):Play()
+	end)
+end
+
+btn('Scripts', 'rbxassetid://5970247019', function()
+
+end)
 
 local is_open = true
 game:GetService('UserInputService').InputBegan:Connect(function(i)
@@ -122,8 +305,6 @@ game:GetService('UserInputService').InputBegan:Connect(function(i)
 		end
 	end
 end)
-
-print('Press Delete to toggle mollermethod')
 
 API.play(assets.startup)
 
