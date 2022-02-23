5a7738b30ba1f57a5cb74dd90dfcc7faa08074ec
Jack <29169102+Jack5079@users.noreply.github.com>
2022-02-22 20:51:24 -0500

    daniel stenberg should talk to the guy with the lightning bolts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-curl -o ./rojo.zip https://github.com/rojo-rbx/rojo/releases/download/v7.0.0/rojo-7.0.0-linux.zip
+curl -L -o ./rojo.zip https://github.com/rojo-rbx/rojo/releases/download/v7.0.0/rojo-7.0.0-linux.zip
 unzip ./rojo.zip
 npx rbxtsc --type model
 chmod +x ./rojo/rojo
