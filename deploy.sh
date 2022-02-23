b9e1a86e884d8cb689962a1d370745f8d6922165
Jack <29169102+Jack5079@users.noreply.github.com>
2022-02-22 20:40:35 -0500

    just in case idk

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 curl -o rojo.zip https://github.com/rojo-rbx/rojo/releases/download/v7.0.0/rojo-7.0.0-linux.zip
 unzip rojo.zip
 npx rbxtsc --type model
-
+chmod +x ./rojo/rojo
 ./rojo/rojo build default.project.json -o src/mollermethod.rbxmx
