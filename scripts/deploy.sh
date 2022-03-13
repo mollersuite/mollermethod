4bf2e5841312e876a19173ec2b41b3d06fd23aec
Jack <29169102+Jack5079@users.noreply.github.com>
2022-03-13 13:40:07 -0400

    ci: :ambulance: PLEASE WORK I BEG YOU :moller:

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 curl -L https://github.com/Roblox/foreman/releases/download/v1.0.3/foreman-1.0.3-linux.zip | zcat >> foreman
 chmod +x foreman
-foreman install
+./foreman install
 npx pnpm run build -r
