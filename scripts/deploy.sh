a62e9e619e5de3f2829cc7ef71b08b35b5f5a697
Jack <29169102+Jack5079@users.noreply.github.com>
2022-03-13 13:47:21 -0400

    ci: :ambulance: if this doesnt work then i give up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 curl -L https://github.com/Roblox/foreman/releases/download/v1.0.3/foreman-1.0.3-linux.zip | zcat >> foreman
 chmod +x foreman
 ./foreman install
+export PATH=~/.foreman/bin:$PATH
 npx pnpm run build -r
