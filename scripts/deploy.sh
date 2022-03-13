35e5c655f094976fc09d8abb6ca16f16600aae94
Jack <29169102+Jack5079@users.noreply.github.com>
2022-03-13 14:48:41 -0400

    revert: delete orca style fuck that

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,4 @@ curl -L https://github.com/Roblox/foreman/releases/download/v1.0.3/foreman-1.0.3
 chmod +x foreman
 ./foreman install
 export PATH=~/.foreman/bin:$PATH
-npx pnpm run build --filter ./roblox
-remodel run scripts/bundle.lua ./website/lib/loader.client.lua
-npx pnpm run build --filter ./website
+npx pnpm run build -r
--- scripts/minify.js
+++
@@ -1,8 +0,0 @@
-import luamin from "luamin"
-import { resolve } from "path"
-import fs from "fs"
-import { fileURLToPath } from "url"
-
-const path = resolve(fileURLToPath(import.meta.url), "..", "bundle.tmp")
-const result = luamin.minify(fs.readFileSync(path), "utf8")
-fs.writeFileSync("scripts/bundle.tmp", result)
