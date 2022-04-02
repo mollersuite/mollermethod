cc8b24da2454438b5c324318cbcede13f6bea921
trollar <70556971+trollar@users.noreply.github.com>
2022-04-02 04:45:27 +0100

    docs: :memo: adds faq
    coauthored by Jack

--- website/routes/__layout.svelte
+++ website/routes/__layout.svelte
@@ -6,6 +6,7 @@
 	$: path = $page.url.pathname
 	import Settings from "@fluentui/svg-icons/icons/clipboard_settings_20_regular.svg?raw"
 	import Cube from "@fluentui/svg-icons/icons/cube_20_regular.svg?raw"
+	import BookQuestionMark from "@fluentui/svg-icons/icons/book_question_mark_20_regular.svg?raw"
 </script>
 
 <Header>
@@ -18,6 +19,9 @@
 	</Button>
 	<Button href="/api" label="API" selected={path === "/api"}>
 		{@html Cube}
+	</Button>
+	<Button href="/faq" label="FAQ" selected={path === "/faq"}>
+		{@html BookQuestionMark}
 	</Button>
 	<Button href="https://discord.gg/HAw7Zf8GF5" label="Discord">
 		<svg role="img" viewBox="0 0 24 24" width="20" height="20" xmlns="http://www.w3.org/2000/svg"
