cac043ff4a867329517a0e2b40c90247bf538cc3
Jack <29169102+Jack5079@users.noreply.github.com>
2022-04-06 12:09:53 -0400

    fix: :speech_balloon: add year to latest quote
    how did i fuck this up

--- roblox/src/strings.ts
+++ roblox/src/strings.ts
@@ -1,5 +1,5 @@
 export const QUOTES = [
-	'"Is it possible to use your gui script stuff on Xbox ?" - JFK#1292',
+	'"Is it possible to use your gui script stuff on Xbox ?" - JFK#1292, 2022',
 	'"how can i do meth" - Charlie, 2022',
 	'"you getting bang lol" - Voiletfissy, 2022',
 	'"new acerus x script inf yeild" - Bro_PlaysRBLX, 2022',
