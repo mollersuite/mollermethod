eab5d8b33ab3fac08155a67749d67b9a205a7527
trollar <70556971+trollar@users.noreply.github.com>
2022-04-06 07:26:28 +0100

    refactor: :label: Adds lots of type definitions and adds in typing for writedialog and readdialog

--- roblox/src/ambient.d.ts
+++ roblox/src/ambient.d.ts
@@ -19,6 +19,11 @@ interface DataModel {
  * @since Infinite Yield 3.8
  */
 declare const IY_LOADED: true | void
+
+/**
+ * Returns a container made for GUI's to be parented in. It hides children from FindFirstChild attacks and does not fire game.DescendantAdded.
+ */
+
 declare const gethui: (() => Instance) | void
 
 /**
@@ -26,12 +31,23 @@ declare const gethui: (() => Instance) | void
  * The `Toggle` argument must be either 0 or 1 (for fire/unfire).
  *
  * **Note:** The `ToTouch` argument must have a child with class `TouchTransmitter` in order for this function to work.
+ * @param ToTouch The `Part` to touch the Part with.
+ * @param Toggle The toggle value which determines whether to fire(0) or unfire(1) the event.
+ * @param Part The `Part` to touch.
  */
+
 declare const firetouchinterest: ((Part: Instance, ToTouch: BasePart, Toggle: 0 | 1) => void) | void
+
+/**
+ * Sets the clipboard to the text you specified.
+ * @param text The text to set the clipboard to. 
+*/
+
 declare const setclipboard: ((text: string) => void) | void
 interface LogService {
 	/**
 	 * Runs scripts on the server, LOL.
+	 * @param script The source of the script to run.
 	 **/
 	ExecuteScript(script: string): void
 }
@@ -39,14 +55,71 @@ interface LogService {
 // Console APIS
 
 // In both exploits
-declare const rconsoleprint: ((message: string) => void) | void
+
+/**
+ * Prints message to the console and takes an optional colour argument. If none is given, the default will be white.
+ * @param message The message to print to the console.
+*/
+
+declare const rconsoleprint: ((message: string, colour?: string) => void) | void
+
+/**
+ * Clears the console.
+*/
+
 declare const rconsoleclear: (() => void) | void
+
+/**
+ * Allows the user to type something into the console.
+ */
+
 declare const rconsoleinput: (() => string) | void
 
 // We will need to detect which one exists, first one is S-W, second is Synapse
+
+/**
+ * Set the console's title to the one provided.
+ * @param title The title to set the console to.
+*/
+
 declare const rconsolesettitle: ((title: string) => void) | void
+
+/**
+ * Set the console's title to the one provided.
+ * @param title The title to set the console to.
+*/
+
 declare const rconsolename: ((title: string) => void) | void
 
 // S-W exclusive
+
+/**
+ * Opens a console window.
+ */
+
 declare const rconsolecreate: (() => void) | void
+
+/**
+ * Closes the console window.
+*/
+
 declare const rconsoledestroy: (() => void) | void
+
+// Filesystem API (S-W)
+
+/**
+ * Makes a write file dialog with title and filter.
+ * @param title The title of the dialog
+ * @param filter File filter, e.g. "*.txt"
+ * @param data The data to be saved
+ */
+
+declare const writedialog: ((title: string, filter: string, data: string) => boolean) | void
+
+/**
+ * Opens a read file dialog with title and filter.
+ * @param title The title of the dialog
+ * @param filter File filter, e.g. "*.txt"
+ */
+
+declare const readdialog: ((title: string, filter: string) => boolean | string) | void
