d257a45b954b3e6015983e2a1a5427a9e046e949
Jack <29169102+Jack5079@users.noreply.github.com>
2022-04-17 16:54:22 -0400

    feat: :technologist: studio generates random theme
    so i know themes will work

--- roblox/serve/run.client.lua
+++ roblox/serve/run.client.lua
@@ -4,7 +4,28 @@ GUI.IgnoreGuiInset = true
 GUI.ResetOnSpawn = false
 GUI.DisplayOrder = 7
 
+-- Generate a random theme with accent, background, and foreground colors, all in hex
+local function generateTheme()
+	local theme = {}
+	theme.accent = string.format("%x", math.random(0, 0xFFFFFF))
+	theme.background = string.format("%x", math.random(0, 0xFFFFFF))
+	theme.foreground = string.format("%x", math.random(0, 0xFFFFFF))
+	return theme
+end
+local theme
+while true do
+	local worked = pcall(function ()
+		theme = generateTheme()
+		Color3.fromHex(theme.accent)
+		Color3.fromHex(theme.background)
+		Color3.fromHex(theme.foreground)
+	end)
+	if worked then
+		break
+	end
+end
 require(game:GetService('ReplicatedStorage').mollermethod) {
 	debug = true,
-	gui = GUI
+	gui = GUI,
+	theme = generateTheme()
 }
