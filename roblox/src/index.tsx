b4b93b0f49ed648685ea19ab4be57f7ff7a38065
Jack <29169102+Jack5079@users.noreply.github.com>
2022-04-17 21:33:42 -0400

    refactor: :recycle: init is now a class
    i will use this to replace several contexts soon

--- roblox/src/index.tsx
+++ roblox/src/index.tsx
@@ -14,31 +14,12 @@ import AdminBail from "bail"
 import type { Plugin } from "types"
 import type { IYConfig } from "Bracket/iy/types"
 import Expletive from "Trendsetter/Expletive"
+
 declare const script: ModuleScript & {
 	plugins: Folder
 }
 
-/**
- * @see https://mthd.ml
- * @name mollermethod
- * @description Scripting, unleashed.
- * @author Jack W.
- * @example
- * ```lua
- * loadstring (game:HttpGet "mthd.ml") {
- *
- * }
- * ```
- */
-export = async function ({
-	gui: GUI,
-	bracket_external,
-	bracket_toggle,
-	debug,
-	plugins: plugin_sources = [],
-	theme,
-	volume = 5,
-}: {
+interface Config {
 	debug?: true
 	gui: ScreenGui
 	bracket_toggle?: Enum.KeyCode
@@ -50,165 +31,181 @@ export = async function ({
 		accent: string
 	}
 	volume?: number
-}) {
-	debug && warn(`starting init`)
-	if (theme) {
-		colors.ACCENT = Color3.fromHex(theme.accent)
-		colors.WHITE = Color3.fromHex(theme.foreground)
-		colors.BLACK = Color3.fromHex(theme.background)
-	}
-	set_volume(volume)
-	// @ts-expect-error
-	if (getcustomasset ?? getsynasset) {
-		task.defer(() => {
-			if (isfile("mollermethod_Blog-Sound-1.ogg")) {
-				play((getcustomasset ?? getsynasset)("mollermethod_Blog-Sound-1.ogg"))
-			} else {
-				play(asset("https://ubuntu.com/wp-content/uploads/2012/02/Blog-Sound-1.ogg"))
-			}
-		})
-	} else {
-		play("rbxassetid://9344041257")
-	}
+}
+/**
+ * @see https://mthd.ml
+ * @name mollermethod
+ * @description Scripting, unleashed.
+ * @author Etcetera (mollersuite division)
+ * @example
+ * ```lua
+ * loadstring (game:HttpGet "mthd.ml") {
+ *
+ * }
+ * ```
+ */
+class mollermethod {
+	container: ScreenGui
+	notif_holder: Frame
+	plugins: Plugin[] = []
+	tree: Roact.Tree
 
-	const plugins: Plugin[] = []
-	const notif_holder = (() => {
-		const notifications = new Instance("Frame", GUI)
-		notifications.Name = "notifications"
-		notifications.AnchorPoint = new Vector2(1, 0)
-		notifications.BackgroundColor3 = Color3.fromHex("#FFFFFF")
-		notifications.BackgroundTransparency = 1
-		notifications.Position = UDim2.fromScale(1, 0)
-		notifications.Size = UDim2.fromScale(1, 1)
+	constructor(public config: Config) {
+		this.config = config
+		if (config.theme) {
+			colors.ACCENT = Color3.fromHex(config.theme.accent)
+			colors.WHITE = Color3.fromHex(config.theme.foreground)
+			colors.BLACK = Color3.fromHex(config.theme.background)
+		}
+		this.container = this.config.gui
+		set_volume(this.config.volume ?? 5)
 
-		const uIPadding = new Instance("UIPadding")
-		uIPadding.Name = "uIPadding"
-		uIPadding.PaddingBottom = new UDim(0, 15)
-		uIPadding.PaddingRight = new UDim(0, 15)
-		uIPadding.Parent = notifications
+		// @ts-expect-error
+		if (getcustomasset ?? getsynasset) {
+			task.defer(() => {
+				if (isfile("mollermethod_Blog-Sound-1.ogg")) {
+					play((getcustomasset ?? getsynasset)("mollermethod_Blog-Sound-1.ogg"))
+				} else {
+					play(asset("https://ubuntu.com/wp-content/uploads/2012/02/Blog-Sound-1.ogg"))
+				}
+			})
+		} else {
+			play("rbxassetid://9344041257")
+		}
 
-		const uIListLayout = new Instance("UIListLayout")
-		uIListLayout.Name = "uIListLayout"
-		uIListLayout.Padding = new UDim(0, 10)
-		uIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-		uIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-		uIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-		uIListLayout.Parent = notifications
-		return notifications
-	})()
+		this.notif_holder = this.setup_notifs()
+		this.load_plugins()
 
+		// Startup sound
+		// @ts-expect-error
+		if (getcustomasset ?? getsynasset) {
+			task.defer(() => {
+				if (isfile("mollermethod_Blog-Sound-1.ogg")) {
+					play((getcustomasset ?? getsynasset)("mollermethod_Blog-Sound-1.ogg"))
+				} else {
+					play(asset("https://ubuntu.com/wp-content/uploads/2012/02/Blog-Sound-1.ogg"))
+				}
+			})
+		} else {
+			play("rbxassetid://9344041257")
+		}
 
-	debug && warn(`loading ${plugin_sources.size()} plugins from settings`)
-	await Promise.allSettled(
-		plugin_sources.map(async source => {
-			const [plugin, err] = loadstring(source)
-			assert(plugin, err)
-
-			plugins.push(
-				plugin({
-					notify: (
-						name: string,
-						description: string,
-						icon: "Error" | "Info" | "Success" | "Warning",
-						duration: number,
-						callback?: Callback
-					) =>
-						Notification.new(name, description, icon, duration, notif_holder, callback),
-					GUI,
-					colors,
-					Snapdragon,
-				})
-			)
-		})
-	)
-
-	debug && warn(`loading default plugins`)
-	await Promise.allSettled(
-		script.plugins.GetDescendants().map(async module => {
-			if (module.IsA("ModuleScript")) {
-				const plugin = require(module) as (opts: any) => Plugin
-				plugins.push(
-					plugin({
-						notify: (
-							name: string,
-							description: string,
-							icon: "Error" | "Info" | "Success" | "Warning",
-							duration: number,
-							callback?: Callback
-						) =>
-							Notification.new(
-								name,
-								description,
-								icon,
-								duration,
-								notif_holder,
-								callback
-							),
-						GUI,
-						colors,
-						Snapdragon,
-					})
-				)
-			} else return
-		})
-	)
-
-	// Load IY plugins
-	if (isfile?.("IY_FE.iy")) {
-		debug && warn(`loading iy plugins`)
-		const config = HttpService.JSONDecode(readfile("IY_FE.iy")) as IYConfig
-		await Promise.allSettled(
-			config.PluginsTable.map(async plugin_path =>
-				iy_to_bracket(readfile(plugin_path), notif_holder, plugins)
-			)
-		)
-	}
-
-	debug && warn(`starting UI`)
-	const tree = Roact.mount(
-		<>
-			{/* UIs */}
-			<Plugins.Provider value={plugins}>
+		this.tree = Roact.mount(
+			<Plugins.Provider value={this.plugins}>
 				<Kill.Provider
 					value={() => {
-						Roact.unmount(tree)
-						GUI.Destroy()
+						Roact.unmount(this.tree)
+						this.container.Destroy()
 					}}>
-					{bracket_external ? (
+					{/* UIs */}
+					{this.config.bracket_external ? (
 						<BracketExternal />
 					) : (
 						<Bracket
 							Key="Bracket"
-							button={bracket_toggle ?? Enum.KeyCode.LeftBracket}
+							button={this.config.bracket_toggle ?? Enum.KeyCode.LeftBracket}
 						/>
 					)}
 					<Trendsetter Key="Menu" />
 					<Expletive Key="Taskbar" />
 				</Kill.Provider>
-			</Plugins.Provider>
-			{/* Services */}
-			<AdminBail container={notif_holder} />
-		</>,
-		GUI
-	)
-	debug && warn(`done`)
-	/*
-		Google Assistant-style startup animation
-	 */
-	const border = new Instance("Frame", GUI)
-	Debris.AddItem(border, 1)
-	border.Size = UDim2.fromScale(1, 1)
-	border.AnchorPoint = new Vector2(0.5, 0.5)
-	border.Position = UDim2.fromScale(0.5, 0.5)
-	border.BackgroundTransparency = 1
-	const stroke = new Instance("UIStroke", border)
-	stroke.Thickness = 15
-	stroke.Color = colors.ACCENT
-	TweenService.Create(
-		border,
-		new TweenInfo(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, true),
-		{
-			Size: new UDim2(1, -10, 1, -10),
+				{/* Services */}
+				<AdminBail container={this.notif_holder} />
+			</Plugins.Provider>,
+			this.container
+		)
+		debug && warn(`done`)
+
+		// Google Assistant-style startup animation
+		const border = new Instance("Frame", this.container)
+		Debris.AddItem(border, 1)
+		border.Size = UDim2.fromScale(1, 1)
+		border.AnchorPoint = new Vector2(0.5, 0.5)
+		border.Position = UDim2.fromScale(0.5, 0.5)
+		border.BackgroundTransparency = 1
+		const stroke = new Instance("UIStroke", border)
+		stroke.Thickness = 15
+		stroke.Color = colors.ACCENT
+		TweenService.Create(
+			border,
+			new TweenInfo(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, true),
+			{
+				Size: new UDim2(1, -10, 1, -10),
+			}
+		).Play()
+	}
+
+	async load_plugins() {
+		const util = {
+			notify: (
+				name: string,
+				description: string,
+				icon: "Error" | "Info" | "Success" | "Warning",
+				duration: number,
+				callback?: Callback
+			) => Notification.new(name, description, icon, duration, this.notif_holder, callback),
+			GUI: this.container,
+			colors,
+			Snapdragon,
 		}
-	).Play()
+
+		if (this.config.plugins) {
+			debug && warn(`loading ${this.config.plugins.size()} plugins from settings`)
+			await Promise.allSettled(
+				this.config.plugins.map(async source => {
+					const [plugin, err] = loadstring(source)
+					assert(plugin, err)
+
+					this.plugins.push(plugin(util))
+				})
+			)
+		}
+
+		debug && warn(`loading default plugins`)
+		await Promise.allSettled(
+			script.plugins.GetDescendants().map(async module => {
+				if (module.IsA("ModuleScript")) {
+					const plugin = require(module) as (opts: any) => Plugin
+					this.plugins.push(plugin(util))
+				} else {
+					return
+				}
+			})
+		)
+
+		// Load IY plugins
+		if (isfile?.("IY_FE.iy")) {
+			debug && warn(`loading iy plugins`)
+			const config = HttpService.JSONDecode(readfile("IY_FE.iy")) as IYConfig
+			await Promise.allSettled(
+				config.PluginsTable.map(async plugin_path =>
+					iy_to_bracket(readfile(plugin_path), this.notif_holder, this.plugins)
+				)
+			)
+		}
+	}
+	setup_notifs() {
+		const holder = new Instance("Frame", this.container)
+		holder.AnchorPoint = new Vector2(1, 0)
+		holder.BackgroundColor3 = Color3.fromHex("#FFFFFF")
+		holder.BackgroundTransparency = 1
+		holder.Position = UDim2.fromScale(1, 0)
+		holder.Size = UDim2.fromScale(1, 1)
+
+		const padding = new Instance("UIPadding")
+		padding.PaddingBottom = new UDim(0, 15)
+		padding.PaddingRight = new UDim(0, 15)
+		padding.Parent = holder
+
+		const uIListLayout = new Instance("UIListLayout")
+		uIListLayout.Padding = new UDim(0, 10)
+		uIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+		uIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+		uIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+		uIListLayout.Parent = holder
+
+		return holder
+	}
 }
+
+export = (config: Config) => new mollermethod(config)
