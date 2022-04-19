0aa11556e895b558daadde343403ec0f4f138c44
Jack <29169102+Jack5079@users.noreply.github.com>
2022-04-18 22:59:20 -0400

    feat(bracket): :sparkles: Strafing on invisible & fly
    Taken from Orca :/

--- roblox/src/Bracket/commands.tsx
+++ roblox/src/Bracket/commands.tsx
@@ -1,8 +1,158 @@
-import { HttpService, TeleportService, Players, Workspace, UserInputService } from "@rbxts/services"
+import {
+	HttpService,
+	TeleportService,
+	Players,
+	Workspace,
+	UserInputService,
+	RunService,
+} from "@rbxts/services"
+import { GroupMotor, Spring } from "@rbxts/flipper"
 import { join_code, play } from "util"
 import Roact from "@rbxts/roact"
 import type { Command } from "types"
 const Player = Players.LocalPlayer
+
+// Taken from Orca: https://github.com/richie0866/orca/blob/b208c76f388cda5e8c960d48c42fb9fc94eb8874/src/jobs/character/flight.client.ts
+namespace FlyService {
+	const player = Players.LocalPlayer
+	const moveDirection = {
+		forward: new Vector3(),
+		backward: new Vector3(),
+		left: new Vector3(),
+		right: new Vector3(),
+		up: new Vector3(),
+		down: new Vector3(),
+	}
+
+	// Whether the player is currently flying, the speed in studs per second, and
+	// the movement direction.
+	let enabled = false
+	let speed = 16
+
+	// The root part and current CFrame. Undefined when there is no character.
+	let humanoidRoot: BasePart | undefined
+	let coordinate: CFrame | undefined
+	let coordinateSpring = new GroupMotor([0, 0, 0], false)
+
+	async function main() {
+		UserInputService.InputBegan.Connect((input, gameProcessed) => {
+			if (gameProcessed) {
+				return
+			}
+			updateDirection(input.KeyCode, true)
+		})
+
+		UserInputService.InputEnded.Connect(input => {
+			updateDirection(input.KeyCode, false)
+		})
+
+		RunService.Heartbeat.Connect(deltaTime => {
+			if (enabled && humanoidRoot && coordinate) {
+				updateCoordinate(deltaTime)
+				coordinateSpring.setGoal([
+					new Spring(coordinate.X),
+					new Spring(coordinate.Y),
+					new Spring(coordinate.Z),
+				])
+				coordinateSpring.step(deltaTime)
+
+				const [x, y, z] = coordinateSpring.getValue()
+				humanoidRoot.AssemblyLinearVelocity = new Vector3()
+				humanoidRoot.CFrame = Workspace.CurrentCamera!.CFrame.Rotation.add(
+					new Vector3(x, y, z)
+				)
+			}
+		})
+
+		// Update root part CFrame with the Camera's current direction. May be removed
+		// in the future.
+		RunService.RenderStepped.Connect(() => {
+			if (enabled && humanoidRoot && coordinate) {
+				humanoidRoot.CFrame = Workspace.CurrentCamera!.CFrame.Rotation.add(
+					humanoidRoot.CFrame.Position
+				)
+			}
+		})
+
+		player.CharacterAdded.Connect(() => {
+			enabled = false
+		})
+	}
+
+	function getUnitDirection() {
+		let sum = new Vector3()
+		for (const [, v3] of pairs(moveDirection)) {
+			sum = sum.add(v3)
+		}
+		return sum.Magnitude > 0 ? sum.Unit : sum
+	}
+
+	function resetCoordinate() {
+		if (!humanoidRoot) {
+			return
+		}
+		const { XVector, YVector, ZVector } = Workspace.CurrentCamera!.CFrame
+		coordinate = CFrame.fromMatrix(humanoidRoot.Position, XVector, YVector, ZVector)
+	}
+
+	function resetSpring() {
+		if (!coordinate) {
+			return
+		}
+		coordinateSpring = new GroupMotor([coordinate.X, coordinate.Y, coordinate.Z], false)
+	}
+
+	function updateCoordinate(deltaTime: number) {
+		if (!coordinate) {
+			return
+		}
+
+		const { XVector, YVector, ZVector } = Workspace.CurrentCamera!.CFrame
+		const direction = getUnitDirection()
+
+		if (direction.Magnitude > 0) {
+			const { X, Y, Z } = direction.mul(speed * deltaTime)
+			coordinate = CFrame.fromMatrix(coordinate.Position, XVector, YVector, ZVector).mul(
+				new CFrame(X, Y, Z)
+			)
+		} else {
+			coordinate = CFrame.fromMatrix(coordinate.Position, XVector, YVector, ZVector)
+		}
+	}
+
+	function updateDirection(code: Enum.KeyCode, begin: boolean) {
+		switch (code) {
+			case Enum.KeyCode.W:
+				moveDirection.forward = begin ? new Vector3(0, 0, -1) : new Vector3()
+				break
+			case Enum.KeyCode.S:
+				moveDirection.backward = begin ? new Vector3(0, 0, 1) : new Vector3()
+				break
+			case Enum.KeyCode.A:
+				moveDirection.left = begin ? new Vector3(-1, 0, 0) : new Vector3()
+				break
+			case Enum.KeyCode.D:
+				moveDirection.right = begin ? new Vector3(1, 0, 0) : new Vector3()
+				break
+			case Enum.KeyCode.Q:
+				moveDirection.up = begin ? new Vector3(0, -1, 0) : new Vector3()
+				break
+			case Enum.KeyCode.E:
+				moveDirection.down = begin ? new Vector3(0, 1, 0) : new Vector3()
+				break
+		}
+	}
+	main().catch(warn)
+	export const toggle = (val: boolean, root: BasePart, spd = 100) => {
+		enabled = val
+		speed = spd
+		humanoidRoot = root
+		if (enabled) {
+			resetCoordinate()
+			resetSpring()
+		}
+	}
+}
 
 export const exit: Command = {
 	description: "Closes the game.",
@@ -48,54 +198,15 @@ export const support: Command = {
 let Flight = false
 export const fly: Command = {
 	description: "Toggles fly mode.",
-	async execute() {
+	async execute(args) {
 		const Character = Player.Character
 		assert(Character, "Character not found")
 		Flight = !Flight
-		if (Flight) {
-				const Humanoid = Character.FindFirstChildWhichIsA("Humanoid")!
-				const Root = Humanoid.RootPart!
-				const BodyGyro = new Instance("BodyGyro", Root)
-				BodyGyro.P = 9e4
-				BodyGyro.MaxTorque = Vector3.one.mul(9e9)
-				BodyGyro.CFrame = Root.CFrame
-				const BodyVelocity = new Instance("BodyVelocity", Root)
-				BodyVelocity.Velocity = Vector3.zero
-				BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-				Humanoid!.PlatformStand = true
-				let moving = false
-				UserInputService.InputBegan.Connect((input, gpe) => {
-					if (!gpe) {
-						moving = true
-						if (input.KeyCode === Enum.KeyCode.W) {
-							while (moving) {
-								BodyVelocity.Velocity = Workspace!.CurrentCamera!.CFrame.LookVector.mul(200)
-								task.wait()
-							}
-						}
-						if (input.KeyCode === Enum.KeyCode.S) {
-							while (moving) {
-								BodyVelocity.Velocity = Workspace!.CurrentCamera!.CFrame.LookVector.mul(-200)
-								task.wait()
-							}
-						}
-					}
-				})
-				UserInputService.InputEnded.Connect((input, gpe) => {
-					if (!gpe) {
-						if (input.KeyCode === Enum.KeyCode.W || input.KeyCode === Enum.KeyCode.S) {
-							moving = false
-							BodyVelocity.Velocity = Vector3.zero
-						}
-					}
-				})
-		} else {
-				const Humanoid = Character.FindFirstChildWhichIsA("Humanoid")!
-				const Root = Humanoid.RootPart!
-				Root.FindFirstChildWhichIsA("BodyGyro")?.Destroy()
-				Root.FindFirstChildWhichIsA("BodyVelocity")?.Destroy()
-				Humanoid.PlatformStand = false
-		}
+		FlyService.toggle(
+			Flight,
+			Character.FindFirstChildOfClass("Humanoid")!.RootPart!,
+			tonumber(args[0])
+		)
 	},
 }
 
@@ -106,38 +217,39 @@ export const fish: Command = {
 		assert(Character, "Character not found")
 		Flight = !Flight
 		if (Flight) {
-				const Humanoid = Character.FindFirstChildWhichIsA("Humanoid")!
-				const Root = Humanoid.RootPart!
-				const BodyVelocity = new Instance("BodyVelocity", Root)
-				BodyVelocity.Velocity = Vector3.zero
-				BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-				Humanoid!.PlatformStand = true
-				let moving = false
-				UserInputService.InputBegan.Connect((input, gpe) => {
-					if (!gpe) {
-						moving = true
-						if (input.KeyCode === Enum.KeyCode.W) {
-							BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-							while (moving) {
-								BodyVelocity.Velocity = Workspace!.CurrentCamera!.CFrame.LookVector.mul(200)
-								task.wait()
-							}
+			const Humanoid = Character.FindFirstChildWhichIsA("Humanoid")!
+			const Root = Humanoid.RootPart!
+			const BodyVelocity = new Instance("BodyVelocity", Root)
+			BodyVelocity.Velocity = Vector3.zero
+			BodyVelocity.MaxForce = Vector3.one.mul(9e9)
+			Humanoid!.PlatformStand = true
+			let moving = false
+			UserInputService.InputBegan.Connect((input, gpe) => {
+				if (!gpe) {
+					moving = true
+					if (input.KeyCode === Enum.KeyCode.W) {
+						BodyVelocity.MaxForce = Vector3.one.mul(9e9)
+						while (moving) {
+							BodyVelocity.Velocity =
+								Workspace!.CurrentCamera!.CFrame.LookVector.mul(200)
+							task.wait()
 						}
 					}
-				})
-				UserInputService.InputEnded.Connect((input, gpe) => {
-					if (!gpe) {
-						if (input.KeyCode === Enum.KeyCode.W) {
-							moving = false
-							BodyVelocity.MaxForce = Vector3.zero
-						}
+				}
+			})
+			UserInputService.InputEnded.Connect((input, gpe) => {
+				if (!gpe) {
+					if (input.KeyCode === Enum.KeyCode.W) {
+						moving = false
+						BodyVelocity.MaxForce = Vector3.zero
 					}
-				})
+				}
+			})
 		} else {
-				const Humanoid = Character.FindFirstChildWhichIsA("Humanoid")!
-				const Root = Humanoid.RootPart!
-				Root.FindFirstChildWhichIsA("BodyVelocity")?.Destroy()
-				Humanoid.PlatformStand = false
+			const Humanoid = Character.FindFirstChildWhichIsA("Humanoid")!
+			const Root = Humanoid.RootPart!
+			Root.FindFirstChildWhichIsA("BodyVelocity")?.Destroy()
+			Humanoid.PlatformStand = false
 		}
 	},
 }
@@ -221,7 +333,7 @@ Roact.mount(
 )
 export const invisible: Command = {
 	description: "Become invisible",
-	async execute() {
+	async execute(args) {
 		const Character = Player.Character as Model
 		assert(Character, "you need a character")
 		Invisible = !Invisible
@@ -237,42 +349,8 @@ export const invisible: Command = {
 			block.CFrame = root.CFrame
 			block.Parent = Workspace
 
-			const BodyGyro = new Instance("BodyGyro", block)
-			BodyGyro.P = 9e4
-			BodyGyro.MaxTorque = Vector3.one.mul(9e9)
-			BodyGyro.CFrame = block.CFrame
-			const BodyVelocity = new Instance("BodyVelocity", block)
-			BodyVelocity.Velocity = Vector3.zero
-			BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-			let movingpart = false
-			UserInputService.InputBegan.Connect((input, gpe) => {
-				if (!gpe) {
-					movingpart = true
-					if (input.KeyCode === Enum.KeyCode.W) {
-						BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-						while (movingpart) {
-							BodyVelocity.Velocity = Workspace!.CurrentCamera!.CFrame.LookVector.mul(200)
-							task.wait()
-						}
-					}
-					if (input.KeyCode === Enum.KeyCode.S) {
-						BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-						while (movingpart) {
-							BodyVelocity.Velocity = Workspace!.CurrentCamera!.CFrame.LookVector.mul(-200)
-							task.wait()
-						}
-					}
-				}
-			})
-			UserInputService.InputEnded.Connect((input, gpe) => {
-				if (!gpe) {
-					if (input.KeyCode === Enum.KeyCode.W || input.KeyCode === Enum.KeyCode.S) {
-						movingpart = false
-						BodyVelocity.Velocity = Vector3.zero
-						BodyVelocity.MaxForce = Vector3.one.mul(9e9)
-					}
-				}
-			})
+			FlyService.toggle(true, block, tonumber(args[0]))
+
 			Character.PivotTo(new CFrame(Vector3.one.mul(1000), Vector3.zero))
 			task.spawn(() => {
 				task.wait(1)
@@ -280,6 +358,7 @@ export const invisible: Command = {
 			})
 			Workspace.CurrentCamera!.CameraSubject = block
 		} else {
+			FlyService.toggle(false, block)
 			const humanoid = Character?.FindFirstChildWhichIsA("Humanoid")
 			assert(humanoid, "you need a humanoid")
 			const root = humanoid.RootPart
