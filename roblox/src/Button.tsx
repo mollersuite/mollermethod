e73eb41c2e66ee601f8d9b88e691f7bb7375fe37
Jack <29169102+Jack5079@users.noreply.github.com>
2022-04-20 12:46:10 -0400

    perf(expletive): :zap: no re-renders on button

--- roblox/src/Button.tsx
+++ roblox/src/Button.tsx
@@ -1,6 +1,7 @@
+import { Spring } from "@rbxts/flipper"
 import Roact from "@rbxts/roact"
 import { pure, useState } from "@rbxts/roact-hooked"
-import { useSpring } from "@rbxts/roact-hooked-plus"
+import { useSingleMotor, useSpring } from "@rbxts/roact-hooked-plus"
 import colors from "colors"
 
 export = pure<{
@@ -10,27 +11,26 @@ export = pure<{
 	Activated?: (rbx: TextButton, input: InputObject, clickCount: number) => unknown
 	Accent?: boolean
 }>(({ Text, Activated, Image, LayoutOrder, Accent }) => {
-	const [hovered, setHovered] = useState(false)
-	const [active, setActive] = useState(false)
-
+	const [hovered, setHovered] = useSingleMotor(0)
+	const [scale, setScale] = useSingleMotor(1)
 	return (
 		<textbutton
 			BackgroundColor3={Accent ? colors.ACCENT : colors.BLACK}
 			Text=""
 			LayoutOrder={LayoutOrder}
 			Event={{
-				MouseEnter: () => setHovered(true),
+				MouseEnter: () => setHovered(new Spring(1)),
 				MouseLeave: () => {
-					setHovered(false)
-					setActive(false)
+					setHovered(new Spring(0))
+					setScale(new Spring(1))
 				},
 				Activated,
-				MouseButton1Down: () => setActive(true),
-				MouseButton1Up: () => setActive(false),
+				MouseButton1Down: () => setScale(new Spring(1.1)),
+				MouseButton1Up: () => setScale(new Spring(1)),
 			}}
 			AutomaticSize="X"
 			Size={UDim2.fromOffset(30, 30)}>
-			<uiscale Scale={useSpring(active ? 1.1 : 1, {})} />
+			<uiscale Scale={scale} />
 			<uipadding PaddingLeft={new UDim(0, 10)} PaddingRight={new UDim(0, 10)} />
 			<uicorner CornerRadius={new UDim(0, 10)} />
 			<imagelabel
@@ -47,19 +47,15 @@ export = pure<{
 				TextXAlignment="Left"
 				BackgroundTransparency={1}
 				TextColor3={colors.WHITE}
-				Position={useSpring(hovered ? 1 : 0, {
-					dampingRatio: 1,
-				}).map(n => UDim2.fromScale(0, 0.5).Lerp(new UDim2(0, 25, 0.5, 0), n))}
+				Position={hovered.map(n =>
+					UDim2.fromScale(0, 0.5).Lerp(new UDim2(0, 25, 0.5, 0), n)
+				)}
 				AnchorPoint={new Vector2(0, 0.5)}
 				Font="GothamBlack"
 				Text={Text}
 				// for hover
-				TextSize={useSpring(hovered ? 11.99 : 1, {
-					dampingRatio: 1,
-				})}
-				TextTransparency={useSpring(hovered ? 0 : 1, {
-					dampingRatio: 1,
-				})}
+				TextSize={hovered.map(n => math.max(n * 11.99, 1))}
+				TextTransparency={hovered.map(n => 1 - n)}
 			/>
 		</textbutton>
 	)
