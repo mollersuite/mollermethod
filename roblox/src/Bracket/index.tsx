67d1ce536384669207dc1c2814b546b67771b0b9
Jack <29169102+Jack5079@users.noreply.github.com>
2022-05-04 20:02:01 -0400

    Revert "refactor(bracket): :sparkles: New bracket workflow"
    This reverts commit 6fedc3fda3fb453d5509452a928ab46e632305b5.

--- roblox/src/Bracket/index.tsx
+++ roblox/src/Bracket/index.tsx
@@ -1,13 +1,11 @@
 import colors from "colors"
-import { flat, play, Plugins } from "util"
+import { play, Plugins } from "util"
 import { hooked, useContext, useEffect, useState, useBinding, useRef } from "@rbxts/roact-hooked"
 import { UserInputService } from "@rbxts/services"
 import execute from "./run"
 import Roact from "@rbxts/roact"
 import Suggestions from "./Suggestions"
 import { useSpring } from "@rbxts/roact-hooked-plus"
-import { Export, Argument } from "types"
-import Object from "@rbxts/object-utils"
 
 export const toggle: BindableEvent<(state: boolean) => unknown> = new Instance("BindableEvent")
 
@@ -30,8 +28,6 @@ export const toggle: BindableEvent<(state: boolean) => unknown> = new Instance("
 export default hooked<{ button: Enum.KeyCode; test?: boolean }>(({ button, test }) => {
 	const [shown, setShown] = useBinding(false)
 	const [text, setText] = useState("")
-	const [selected, setSelected] = useState<Export>()
-	const [argument, setArgument] = useState<[name: string, arg: Argument]>()
 	const plugins = useContext(Plugins)
 	const box = useRef<TextBox>()
 
@@ -44,7 +40,7 @@ export default hooked<{ button: Enum.KeyCode; test?: boolean }>(({ button, test 
 				play("rbxassetid://8458409341") // windows 11 hardware connect
 				box.getValue()!.CaptureFocus()
 				task.wait()
-				box.getValue()!.Text = ""
+				box.getValue()!.Text = ''
 			}
 		})
 		return () => input_began.Disconnect()
@@ -79,19 +75,6 @@ export default hooked<{ button: Enum.KeyCode; test?: boolean }>(({ button, test 
 				VerticalAlignment="Top"
 				Padding={new UDim(0, 10)}
 			/>
-			{selected && (
-				<textlabel
-					Text={argument ? `${selected.Name} > ${argument[0]}` : selected.Name}
-					Font="GothamBlack"
-					TextSize={24}
-					TextColor3={colors.ACCENT}
-					TextXAlignment="Left"
-					TextYAlignment="Center"
-					TextTransparency={0.5}
-					BackgroundTransparency={1}
-					Size={new UDim2(1, 0, 0, 30)}
-				/>
-			)}
 			<textbox
 				Key="!"
 				Ref={box}
@@ -108,50 +91,16 @@ export default hooked<{ button: Enum.KeyCode; test?: boolean }>(({ button, test 
 				BackgroundColor3={colors.BLACK}
 				Event={{
 					FocusLost: (rbx, enter) => {
-						if (argument) {
-							setArgument(undefined)
-							setText("")
-							box.getValue()!.CaptureFocus()
-							if (enter) {
-							}
-						} else if (selected) {
-							setSelected(undefined)
-							setText("")
-							box.getValue()!.CaptureFocus()
-							if (enter) {
-							}
-						} else {
-							setShown(false)
-							if (!enter) {
-								play("rbxassetid://8926096648") // windows 11 hardware disconnect
-							} else execute(text, plugins)
+						setShown(false)
+						if (!enter) {
+							play("rbxassetid://8926096648") // windows 11 hardware disconnect
+							return
 						}
+						execute(text, plugins)
 					},
 				}}
 				Change={{
-					Text: rbx => {
-						if (rbx.Text.sub(rbx.Text.size()) === " ") {
-							const name = rbx.Text.sub(0, rbx.Text.size() - 1)
-							if (!selected) {
-								const Export = flat(
-									plugins.mapFiltered(plugin => plugin.Exports)
-								).find(cmd => cmd.Name === name)
-								if (Export && Object.keys(Export.Arguments).size() > 0) {
-									setSelected(Export)
-									setText("")
-								} else {
-									play("rbxassetid://8458408918")
-									setShown(false)
-									box.getValue()!.ReleaseFocus()
-								}
-							} else if (!argument && selected.Arguments[name]) {
-								setArgument([name, selected.Arguments[name]])
-								setText("")
-							}
-						} else {
-							setText(rbx.Text)
-						}
-					},
+					Text: rbx => setText(rbx.Text),
 				}}>
 				{/* Add an invisible button to activate Modal. Fuck you, Roblox. */}
 				<textbutton
@@ -188,13 +137,7 @@ export default hooked<{ button: Enum.KeyCode; test?: boolean }>(({ button, test 
 					/>
 				</uistroke>
 			</textbox>
-			{selected ? (
-				Object.entries(selected.Arguments).map(([name, value]) => (
-					<textlabel AutomaticSize="XY" Text={`${name} (${value.Type})`} />
-				))
-			) : (
-				<Suggestions Text={text} />
-			)}
+			<Suggestions Text={text} />
 		</scrollingframe>
 	)
 })
