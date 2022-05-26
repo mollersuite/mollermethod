d5f8196c2aa4f4eb632a7edc0ed9a505691d67e6
Jack <29169102+Jack5079@users.noreply.github.com>
2022-05-26 15:30:52 -0400

    ci: 🐛 Pin rojo version
    Fuck you foreman

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-curl -L https://github.com/Roblox/foreman/releases/download/v1.0.3/foreman-1.0.3-linux.zip | zcat >> foreman
-chmod +x foreman
-./foreman install
-export PATH=~/.foreman/bin:$PATH
+curl -L https://github.com/rojo-rbx/rojo/releases/download/v7.0.0/rojo-7.0.0-linux.zip | zcat >> rojo
+chmod +x rojo
+mkdir jojo
+mv ./rojo ./jojo/rojo
+export PATH=$PWD/jojo:$PATH
 echo "[moller] Building roblox"
 cd roblox
 npm run build
