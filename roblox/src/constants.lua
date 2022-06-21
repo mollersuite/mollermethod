cf350ed0663d22b0c2a2c1c1697d21b09f46c842
Jack <29169102+Jack5079@users.noreply.github.com>
2022-06-21 19:54:49 -0400

    feat: :speech_balloon: pixei quote
    fuck yoy

--- roblox/src/constants.lua
+++ roblox/src/constants.lua
@@ -1,6 +1,7 @@
 local module = {}
 
 module.QUOTES = {
+	'"get threapy :funnythumbsup:" - pixei, 2022',
 	'"i Do Not Care" - Jack, 2022',
 	[["can't you just compile windows" - Creepdasheep, 2022]],
 	'"Ventoy my beloved" - Charlie, 2022',
