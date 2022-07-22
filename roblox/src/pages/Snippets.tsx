a505b3d94a78ee948fbb01cdc52f1ddb730b2fb4
Jack <29169102+Jack5079@users.noreply.github.com>
2022-07-22 08:25:49 -0400

    feat: :sparkles: Add search bar to Snippets
    Fixes #59

--- roblox/src/pages/Snippets.tsx
+++ roblox/src/pages/Snippets.tsx
@@ -1,10 +1,11 @@
 import Roact from "@rbxts/roact"
-import { pure, useEffect, useState } from "@rbxts/roact-hooked"
+import { pure, useBinding, useContext, useEffect, useState } from "@rbxts/roact-hooked"
 import { HttpService } from "@rbxts/services"
 import Placeholder from "../components/Placeholder"
 import Notification from "Notification"
 import Page from "../components/Page"
 import useColor from "hooks/useColor"
+import { Colors, escape_lua_pattern } from "util"
 
 type Config = {
 	snippets: {
@@ -19,6 +20,11 @@ export = pure<{ holder: Instance }>(({ holder }) => {
 
 	const [snippets, setSnippets] = useState<Config["snippets"]>([])
 	const [editing, setEditing] = useState(false)
+
+	const [query, setQuery] = useBinding("")
+	const [focused, setFocused] = useBinding(false)
+	const [colors] = useContext(Colors)
+
 	useEffect(() => {
 		if (readfile) {
 			setSnippets((HttpService.JSONDecode(readfile("mollermethod.json")) as Config).snippets)
@@ -33,7 +39,7 @@ export = pure<{ holder: Instance }>(({ holder }) => {
 				PaddingBottom={new UDim(0, 10)}
 			/>
 			<textbutton
-				LayoutOrder={-100}
+				LayoutOrder={-102}
 				Size={new UDim2(0, 24, 0, 24)}
 				Text={"+"}
 				Rotation={editing ? 45 : 0}
@@ -59,6 +65,32 @@ export = pure<{ holder: Instance }>(({ holder }) => {
 						VerticalAlignment="Top"
 						SortOrder="LayoutOrder"
 					/>
+					<textbox
+						Text={query}
+						ClearTextOnFocus={false}
+						Change={{
+							Text: rbx => setQuery(rbx.Text),
+						}}
+						Event={{
+							FocusLost: () => setFocused(false),
+							Focused: () => setFocused(true),
+						}}
+						Font="RobotoMono"
+						TextSize={20}
+						TextColor3={white}
+						PlaceholderText="Search for a snippet"
+						PlaceholderColor3={colors.map(colors =>
+							colors.WHITE.Lerp(colors.BLACK, 0.5)
+						)}
+						BackgroundColor3={white}
+						BackgroundTransparency={focused.map(focused => (focused ? 0.5 : 1))}
+						TextXAlignment="Left"
+						BorderSizePixel={0}
+						TextWrapped
+						LayoutOrder={-101}
+						Size={new UDim2(1, 0, 0, 30)}>
+						<uipadding PaddingLeft={new UDim(0, 10)} PaddingRight={new UDim(0, 10)} />
+					</textbox>
 					{snippets.map(snippet => (
 						<textbutton
 							Text={snippet.name}
@@ -71,6 +103,13 @@ export = pure<{ holder: Instance }>(({ holder }) => {
 							TextYAlignment="Center"
 							Size={UDim2.fromScale(1, 0)}
 							Font="Gotham"
+							Visible={query.map(query => {
+								if (query === "") return true
+
+								return (
+									snippet.name.match(escape_lua_pattern(query))[0] !== undefined
+								)
+							})}
 							Event={{
 								Activated: () => {
 									const [run, err] = loadstring(snippet.code)
