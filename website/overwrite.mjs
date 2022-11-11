30b3e0c57f49fb52f2888e1ad3afa647d9b9643b
trollar <70556971+trollar@users.noreply.github.com>
2022-11-11 22:35:10 +0000

    feat(loader): :sparkles: New loader instead of old one

--- website/overwrite.mjs
+++ website/overwrite.mjs
@@ -1,6 +1,6 @@
 // Ugly hack to get a HTML-Lua polyglot with Astro.
 import { readFile, writeFile } from "node:fs/promises"
 console.log("[ugly hack!] Overwriting website/dist/index.html to add polyglot...")
-const loader = await readFile("./loader.client.lua", "utf-8")
+const loader = await readFile("../roblox/init.lua", "utf-8")
 const html = await readFile("./dist/index.html", "utf-8")
 writeFile("./dist/index.html", `--[=[${html}<!--]=]\n${loader}-->`)
