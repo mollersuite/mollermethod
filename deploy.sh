910abadec0d749e370aeb8b740f6ffd132d86c98
charlie <70556971+trollar@users.noreply.github.com>
2022-11-11 22:05:07 +0000

    👷 Update deploy.sh
    Updates the deploy.sh file to not build roblox as roblox-ts is no longer being used

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
-set -euo pipefail # See https://chaos.social/@tamtararam/108653282099480851
-curl -L https://github.com/rojo-rbx/rojo/releases/download/v7.0.0/rojo-7.0.0-linux.zip | zcat >> rojo
-chmod +x rojo
-mkdir jojo
-mv ./rojo ./jojo/rojo
-export PATH=$PWD/jojo:$PATH
-echo "[moller] Building roblox"
-cd roblox
-npm run build
+set -euo pipefail
 cd ../website
 echo "[moller] Building website"
 npm run build
